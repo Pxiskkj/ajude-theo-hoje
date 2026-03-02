1c2f0173c730ffc705cd9d90890bb0f42606a6e1
EduardoD3 <eduardoleite20022@gmail.com>
2026-03-02 09:21:22 -0300

    FEAT: Projeto alterado para javascript para necessidades do checkout e checkout implementado

---
+++ server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname)); // serve index.html
+
+app.post("/criar-pagamento", async (req, res) => {
+  try {
+    const { valor } = req.body;
+
+    const response = await axios.post(
+      "https://api.pluggoucash.com/v1/payments",
+      {
+        amount: valor,
+        description: "Doação - Ajude Theo",
+        payment_method: "pix"
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.PLUGOU_PRIVATE_KEY}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ error: "Erro ao criar pagamento" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
